547bcf194178c822c3fdc455801e07cd55ad9609
kosail <dsolishn@pm.me>
2026-03-15 13:54:12 -0600

    Updated token types. I intentionally omitted IO, FS and such modules.
    Let first build a simple, functional core

--- src/lexer/token_type.zig
+++ src/lexer/token_type.zig
@@ -1,5 +1,61 @@
 pub const TokenType = enum {
+    NEWLINE, // \n
+    INDENT, // identation++
+    DEDENT, // identation--
+    SEMICOLON, // ;
+    COMMA, // ,
+    COLON, // :
+
+    LPAREN, // (
+    RPAREN, // )
+    LBRACKET, // [
+    RBRACKET, // ]
+    LBRACE, // {
+    RBRACE, // }
+
+    IDENTIFIER,
+    INT_LITERAL,
+    FLOAT_LITERAL,
+    STRING_LITERAL,
+    BOOL_LITERAL,
+    NULL_LITERAL,
+
+    PLUS,
+    MINUS,
+    START,
+    SLASH,
+    PERCENT,
+
+    ARROW, // -> (for return types in functions)
+    ASSIGN, // =
+    EQ, // ==
+    NEQ, // !=
+    LT, // <
+    LTE, // <=
+    GT, // >
+    GTE, // >=
+
+    AND,
+    OR,
+    NOT,
+
+    FN, // function
     RETURN,
-    INT_LIT,
-    SEMICOLON
+    IF,
+    ELSE,
+    WHILE,
+    VAR, // variable
+    CONST, // constant
+    TRY,
+    CATCH,
+
+    // To help identifiers to match explicit typing.
+    TYPE_INT, // int
+    TYPE_FLOAT, // float
+    TYPE_BOOL, // boolean
+    TYPE_STRING, // string
+    TYPE_NOTHING, // nothing (AKA void)
+
+    LINE_COMMENT, // //comment
+    BLOCK_COMMENT, // /* comment */
 };
--- src/lexer/tokenizer.zig
+++ src/lexer/tokenizer.zig
@@ -1,8 +1,6 @@
-// This function should return a type Token in the future
+// This function should return a type Token in the future, but for now just prints everything out to confirm the parser works
 pub fn Tokenizer(str: []const 8) {
     for (str, 0..) |ch, index| {
-        if (failure) {
-            
-        }
+        
     }
 }
